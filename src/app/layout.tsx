dda98ae0712ab578568918bb2eefa1847276df2b
Eric Riedel <8659979+emriedel@users.noreply.github.com>
2025-09-03 17:13:59 -0700

    Prevented zooming issues on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: '#000000', // Matches app-bg
 };
 
